447fdef505e95ba98548f4f4c4d2f976f0f85a82
Krishna Nand Gupta <krishna1605gupta@gmail.com>
2024-08-22 16:49:26 +0530

    m2() method is created by dev1

--- test1.java
+++ test1.java
@@ -6,9 +6,16 @@ class Test1
 
 	}
 
+	public void m2(){
+		int x=10;
+		int y=20;
+		System.out.println("Sum of two number is ::"+x+y);
+	}
+
 	public static void main(String args[]){
 		Test1 t1= new Test1();
 		t1.m1();
+		t1.m2();
 	}
 
 
