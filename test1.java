2c551bcbeec5453a79595516f4611937eb32af2e
Krishna Nand Gupta <krishna1605gupta@gmail.com>
2024-08-22 16:56:20 +0530

    After discuss with dev1 final modification done by dev 2

--- test1.java
+++ test1.java
@@ -6,16 +6,15 @@ class Test1
 
 	}
 
-<<<<<<< HEAD
+
 	public void m3(){
 		System.out.println("m3() method start ---dev2");
 
-=======
+
 	public void m2(){
 		int x=10;
 		int y=20;
 		System.out.println("Sum of two number is ::"+x+y);
->>>>>>> 447fdef505e95ba98548f4f4c4d2f976f0f85a82
 	}
 
 	public static void main(String args[]){
